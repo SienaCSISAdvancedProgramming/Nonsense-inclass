04d33a248eecdec3998c7c988eefa785c365b673
Jim Teresco <terescoj@gmail.com>
2021-01-29 17:09:52 -0500

    Starter code.

---
+++ Nonsense.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * In-class Example to refresh a little Java knowledge and to use
+ * input and output redirection.
+ * 
+ * @author Jim Teresco, completed by 
+ * @version Spring 2021
+ */
+public class Nonsense
+{
+
+    public static void main(String args[]) {
+     
+        Scanner s = new Scanner(System.in);
+        
+        s.close();
+    }
+}
